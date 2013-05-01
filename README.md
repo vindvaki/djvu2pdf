1c199f28cf142b5c522be4eae27fd36cc6669670
Hörður Freyr Yngvason <hordurf@gmail.com>
2013-05-01 18:38:47 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+
+# WARNING
+
+This is script is very fragile. Don't use it unless you know what it
+does!
+
+
+# ABOUT
+
+Generates compressed PDF from DjVu and tries to include text layers
+from the original DjVu file. I have no idea what happens in case there
+is no embedded text.
+
+
+# (nontrivial) DEPENDENCIES
+
+- `djvused`: To extract metadata like the TOC and the number of pages.
+- `ddjvu`: To split the djvu file into tiff pages.
+- `djvu2hocr`: To extract the OCR layers for `pdfbeads`.
+- `pdfbeads`: To combine TIFF images and OCR content into a highly
+  compressed pdf file.
+- `djvu2pdf_toc_parser.py`: A python script to convert the TOC for `pdfbeads`.
+
+
+# TODO
+
+## Handle arguments
+
+The basic use case for now is only `djvu2pdf [input] [output]`, but we
+should at least make sure that
+
+- exactly two arguments are provided
+- `input` exists
+- `output` is writable (otherwise we'd lose a lot of precious
+  work)
+  
+Furthermore, it might be nice to have the option to include a
+`pdfbeads`-compatible TOC with the input file (the indentation-based
+syntax is nice, so one might decide to write a TOC). This feature could 
+be introduced through the flag `--toc=[table of contents file]`
+
+
+## Handle errors along the way
+
+- If `input` is not a djvu file, then we should fail instantly.
+- If some dependency isn't installed, we should quit immediately (but
+  it is not our job to make sure they are set up correctly if they are
+  there).
+- If there is no embedded text then we should not output any temporary
+  html files along the way.
---
+++ djvu2pdf_toc_parser.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+
+import sys
+
+def parse_sexp(toc_input, indent_str, i):
+    """
+    Translate TOC in the s-exp format output by ``djvused`` to a
+    format understood by ``pdfbeads``. 
+
+    ``toc_input[i:]`` is the string to parse, and ``indent_str`` is
+    the string of tabulations for our current level in the output
+    TOC. The output from ``djvused`` should not include the
+    '(bookmarks' prefix. Anything up until the opening brace is
+    disregarded, as well as anything after its matching closing brace.
+
+    If the input is badly formatted, weird things will happen.
+    """
+    
+    while True:
+        
+        if toc_input[i] == '(':
+            i += 1
+            i, title = next_quote(toc_input, i)
+            i, page  = next_quote(toc_input, i)
+            
+            # `djvused` outputs page numbers prefixed with '#' (the
+            # second character of the `page` variable) which we don't
+            # want in our output
+            page = page[0]+page[2:] 
+            
+            print("{0}{1} {2}".format(indent_str, title, page))
+            
+            i = parse_sexp(toc_input, indent_str+'\t', i)
+
+        elif toc_input[i] == ')':
+            return i+1
+        
+        i += 1
+
+def next_quote(str, i):
+    """
+    Finds the next substring ``str[k:j]`` of ``str[i:]`` enclosed by
+    non-escaped double-quotes and returns the tuple ``j, res`` where
+    ``res`` is ``str[k:j]`` with escaped double-quotes replaced by
+    single-quotes.
+    """
+    
+    # Find the opening quote. This is simple because we can safely
+    # assume there is only whitespace in between
+    j = i
+    while str[j] != '"':
+        j += 1
+    i = j
+    j += 1
+    output = ['"']
+    
+    # Find the closing quote. This is a bit more involved because the
+    # output may include escaped double quotes, which `pdfbeads`
+    # cannot handle correctly. To circumvent this bug, we replace
+    # every escaped double quote inside the literal with a single quote.
+    while True:
+        if str[j] == '"':
+            if str[j-1] == "\\":
+                output.pop()
+                output += ["'"]
+            else:
+                output += [str[j]]
+                break
+        else:
+            output += [str[j]]
+        j += 1
+        
+    return j+1, ''.join(output)
+
+if __name__ == '__main__':
+    toc_input = sys.stdin.read()
+    
+    # It's possible that the file does not have a table of contents,
+    # in which case we won't read anything at all
+    if len(toc_input) > 0:
+        # We must skip the '(bookmarks' prefix as it doesn't fit the
+        # general pattern expected by `parse_sexp`. 
+        parse_sexp(toc_input[1:], '', 0) 
